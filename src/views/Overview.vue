c4e51dcfc7b1dcb4fe90ceb9e89fa9d1c9694f02
Herz, Andreas <andreas.herz@sap.com>
2022-01-03 15:27:56 +0100

    no message

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,9 +1,37 @@
 <template>
-  <div>
-    Overview
-  </div>
-</template>
+  <v-app>
+  <v-app-bar
+      app
+      color="#E39623"
+      dark
+      shrink-on-scroll
+      prominent
+      src="@/assets/banner.png"
+      height="300"
+      fade-img-on-scroll >
+    <!-- -->
+      <template v-slot:img="{ props }" v-if="false">
+        <v-img
+          v-bind="props"
+          gradient="to top right, rgba(227,150,35,.8), rgba(255,255,255,.1)"
+        ></v-img>
+      </template>
+      <v-app-bar-title class="text-no-wrap app-title" >Camping Electric Calculator</v-app-bar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
 
+  <!-- Sizes your content based upon application components -->
+  <v-main>
+    <!-- Provides the application the proper gutter -->
+    <v-container>
+     <v-container style="height: 1000px;"></v-container>
+    </v-container>
+  </v-main>
+
+  <v-footer app>
+  </v-footer>
+</v-app>
+</template>
 <script>
 
 export default {
@@ -13,6 +41,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style lang="less">
+.app-title {
+  .v-app-bar-title__content  {
+    background-color: rgba(227,150,35,1);
+    padding-left:30px;
+    padding-right: 30px;
+  }
+}
 </style>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: '/CampingBatteryCalculator/'
+  publicPath: '/CampingElectricCalculator/'
 }
