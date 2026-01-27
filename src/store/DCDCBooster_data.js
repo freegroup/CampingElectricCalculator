c30bf324ca13acdafc43ee30bfbc34fabd5ba5b2
Andreas Herz <a.herz@freegroup.de>
2026-01-27 15:07:37 +0100

    dcdc coverter

--- src/store/DCDCBooster_data.js
+++ src/store/DCDCBooster_data.js
@@ -17,7 +17,7 @@ export default
   },
   { 
     uuid: "E7477671-FFBF-4BE4-807F-485E1C1CD2CC", 
-    name: "Victron Energy", 
+    name: "Victron Energy Orion-Tr 24/12-10", 
     description: "Victron Energy Orion-Tr 24/12-10 DC/DC-Wandler 24 V/DC - 12.5 V/DC/12 A 120 W",
     imageSrc: images("./orion_tr_24_12_10.png"),
     exportable: true,
@@ -33,6 +33,26 @@ export default
       strom_out: 10,
       spannung_in: 24,
       spannung_out: 12
+    }
+  },
+  { 
+    uuid: "A8B9C1D2-E3F4-4A5B-9C8D-7E6F5A4B3C2D", 
+    name: "PEKAWAY USB-C Ladegerät 100W", 
+    description: "PEKAWAY Caravan USB-C Netzteil 12/24V 100W, PD 3.0, Metall silber (CAMP 100W USB-C3)",
+    imageSrc: images("./pekaway_100w.png"),
+    exportable: true,
+    shopping: [
+      {
+        shop: "Reichelt",
+        link: "https://www.reichelt.de/de/de/shop/produkt/caravan_-_usb-c_netzteil_12_24_v_100_w_metall_silber-367044",
+        label: "PEKAWAY Caravan USB-C Netzteil 12/24V 100W (CAMP 100W USB-C3)",
+        lastKnownPrice: 67.95
+      }
+    ],
+    data: {
+      strom_out: 5,
+      spannung_in: 12,
+      spannung_out: 20
     }
   }
 ]
